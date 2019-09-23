7c46b2a43e4aeb624009a4fbc5d47f77d7de5ec2
alikurniawan <alikoerniawan02@gmail.com>
2019-09-23 17:34:44 +0700

    yes

--- src/components/channelChose.jsx
+++ src/components/channelChose.jsx
@@ -1,24 +1,39 @@
-import React, { Component } from 'react';
-import {Button} from 'reactstrap';
+import React, { Component } from "react";
+import { Button } from "reactstrap";
 
 class channelChose extends Component {
-    constructor(props){
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.channel1 = this.channel1.bind(this);
-    }
+    this.channel1 = this.channel1.bind(this);
+  }
 
-    channel1(){
-        alert('me')
-    }
+  channel1() {
+    let cha1 = document.getElementById("ch1");
+    let btn1 = document.getElementById("btn1");
 
-    render() { 
-        return ( 
-            <div>
-                <Button className="mt-2" onClick={this.channel1}>OZ Channel</Button>
-            </div>
-         );
+    if(btn1 == true){
+        console.log('hello');
+    }else{
+        console.log('no');
     }
+    
+  }
+
+  // http://listento.ardanradio.com:1059/stream/1/
+
+  render() {
+    return (
+      <div>
+        <Button className="btn1" className="mt-2" onClick={this.channel1}>
+          OZ Channel
+        </Button>
+        <audio id="ch1" contextMenu>
+            <source src="http://listento.ardanradio.com:1059/stream/1/" type="audio/ogg" />
+        </audio>
+      </div>
+    );
+  }
 }
- 
+
 export default channelChose;
