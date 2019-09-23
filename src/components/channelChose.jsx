8ead5787cd8e467f673a3e4f8cd00afbe9595844
alikurniawan <alikoerniawan02@gmail.com>
2019-09-23 23:57:31 +0700

    Masuk2

--- src/components/channelChose.jsx
+++ src/components/channelChose.jsx
@@ -10,23 +10,11 @@ class channelChose extends Component {
 
   channel1() {
     let cha1 = document.getElementById("ch1");
-    let btn1 = document.getElementById("btn1");
-
     let cha2 = document.getElementById("ch2");
-    let btn2 = document.getElementById("btn2");
 
-    let sPlay = cha1.play();
-    let sPlay2 = cha2.play();
+    cha1.play();
 
   }
-
-  channelStop1() {
-    let cha1 = document.getElementById("ch1");
-    let sPause = cha1.pause();
-  }
-
-  // http://listento.ardanradio.com:1059/stream/1/
-  // http://masima.rastream.com/masima-pramborsjakarta?
 
   render() {
     return (
@@ -34,16 +22,12 @@ class channelChose extends Component {
         <Button id="btn1" className="mt-2" onClick={this.channel1}>
           OZ Channel
         </Button>
-        <Button id="bts1" className="mt-2 ml-1" onClick={this.channelStop1}>
-          OZ STOP
-        </Button>
+        
 
         <Button id="btn2" className="mt-2" onClick={this.channel1}>
           OZ Channel2
         </Button>
-        <Button id="bts2" className="mt-2 ml-1" onClick={this.channelStop1}>
-          OZ STOP2
-        </Button>
+       
 
         <audio id="ch1" contextMenu>
           <source src="http://listento.ardanradio.com:1059/stream/1/" type="audio/ogg" />
