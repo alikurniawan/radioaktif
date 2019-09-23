a5f1b95a8d70d694633a23aed928fc84e41b009f
alikurniawan <alikoerniawan02@gmail.com>
2019-09-23 17:06:03 +0700

    first commit

---
+++ src/components/channelChose.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class channelChose extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                ch
+            </div>
+         );
+    }
+}
+ 
+export default channelChose;
